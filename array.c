11525cc2e8107daf48c581e6cf18d12ff7085f48
José Riquelme <jrts@ic.ufal.br>
2025-09-21 17:35:36 -0300

    Adicionado arquivo aula 1

--- array.c
+++ array.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
+void percorrer(int arr[], int i)
+{
+    if (i == 10) return;
+
+    scanf("%d", &arr[i]);
+
+    percorrer(arr, i+1);
+}
+
+
+void escritor(int arr[], int i)
+{
+    if (i == 10) return;
+
+    printf("\nARR[%d] = %d", i, arr[i]);
+
+    escritor(arr, i+1);
+}
+
 int main()
 {
-    int arr[10];
+    // int idade, idade2, idade3;
+
+    // scanf("%d%d%d", &idade, &idade2, &idade3);
+
+    // juntando elementos de um mesmo tipo
+
+    int idades[10];
+
+    // scanf("%d", &idades[6]);
+
+    percorrer(idades, 0);
+
+    escritor(idades, 0);
+
+    // idades[0] -> idade[99];
 }
